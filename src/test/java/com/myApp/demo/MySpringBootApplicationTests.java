e944c6710f591ec6e5ebd2110b117c0c10ad0ec0
Aditya <adityaraj99.a@gmail.com>
2025-07-05 00:58:47 +0530

    My first Commit

---
+++ src/test/java/com/myApp/demo/MySpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myApp.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
